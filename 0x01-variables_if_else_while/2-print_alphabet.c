8f75f2c6555cd41acedaafeebfd17c7310f73cf3
Lexzee <developerlexzee@gmail.com>
2022-04-08 23:09:44 +0100

    fixed errors in script

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,13 @@
  *
  * Return: Always 0 (Success)
  */
- int main(void)
- {
- 	char c;
- 	
- 	for (c = 'a'; c <= 'z'; c++)
- 	putchar(c);
- 	putchar('\n');
+int main(void)
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
 
- 	return (0);
- }
+	return (0);
+}
