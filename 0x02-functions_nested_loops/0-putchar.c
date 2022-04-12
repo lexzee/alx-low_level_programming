38f65f07f78d9e30ef17b53be67ff8ab611bacc4
Lexzee <developerlexzee@gmail.com>
2022-04-12 17:13:11 +0100

    updated scripts

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,15 @@
 *Return zero Always(success)
 */
 
-int main()
+int main(void)
 {
-	_putchar();
+	int i;
+	char p[] = "Holberton";
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
