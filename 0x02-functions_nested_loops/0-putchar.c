dcf35279e4dcf84735d51b013a8f20c51c61413a
Lexzee <developerlexzee@gmail.com>
2022-04-12 23:53:17 +0100

    updated scripts

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,12 +10,12 @@
 int main(void)
 {
 	int i;
-	char p[] = "_putchar";
+	char p[] = "Holberton";
 
 	for (i = 0; i < 9; i++)
-	{
+		{
 		_putchar(p[i]);
-	}
+		}
 		_putchar('\n');
 	return (0);
 }
