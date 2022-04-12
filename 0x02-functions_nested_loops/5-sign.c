217d5117dd9b852521ce9217ed79eb4f3fa329a8
Lexzee <developerlexzee@gmail.com>
2022-04-12 22:29:12 +0100

    updated scripts

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,16 +6,19 @@
 */
 int print_sign(int n)
 {
-	switch (n)
+	if (n > 0)
 	{
-		case (n > 0):
-			_putchar(+);
-			return (1);
-		case (n == 0):
-			_putchar(0);
-			return (0);
-		case (n < 0):
-			_putchar(-);
-			return (-1);
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
 	}
 }
